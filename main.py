94b7fc1a8812ae802df8d1e75306b3782de68a16
seraveist <130033447+seraveist@users.noreply.github.com>
2025-04-07 14:47:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -130,4 +130,4 @@ def dashboard():
     return render_template("stats.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
