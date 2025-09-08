03af4d9d6b596868c8e726843b81312aa36bb6bd
seraveist <130033447+seraveist@users.noreply.github.com>
2025-09-08 19:12:28 +0900

    Update paipu.py

--- src/paipu.py
+++ src/paipu.py
@@ -44,12 +44,9 @@ async def connect():
 
         async with session.get("{}/1/v{}/config.json".format(MS_HOST, version)) as res:
             config = await res.json()
-            url = config["ip"][0]["region_urls"][1]["url"]
-
-        async with session.get(url + "?service=ws-gateway&protocol=ws&ssl=true") as res:
-            servers = await res.json()
-            servers = servers["servers"]
-            server = random.choice(servers)
+            gateways = [g["url"] for g in config["ip"][0]["gateways"]]
+            url = random.choice(gateways)
+            server = re.sub(r'^(?:https?|wss?)://', '', raw_url).rstrip('/')
             endpoint = "wss://{}/gateway".format(server)
 
     channel = MSRPCChannel(endpoint)
